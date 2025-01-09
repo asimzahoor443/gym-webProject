c480c112fec5e9ddc5454479ecc8730efe89419b
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-01-09 13:02:25 +0500

    feat:updated home page

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -1,11 +1,12 @@
 import { SelectedPage } from '@/shared/types';
 import ActionButton from '@/shared/ActionButton';
 import HomePageText from '@/assets/HomePageText.png';
-import HomePageGraphic from '@/assets/HomePageText.png';
-import SponserRedBull from '@/assets/HomePageText.png';
-import SponserForbes from '@/assets/HomePageText.png';
-import SponserFortune from '@/assets/HomePageText.png';
+import HomePageGraphic from '@/assets/HomePageGraphic.png';
+import SponserRedBull from '@/assets/SponsorRedBull.png';
+import SponserForbes from '@/assets/SponsorForbes.png';
+import SponserFortune from '@/assets/SponsorFortune.png';
 import useMediaQuery from '@/hooks/useMediaQuery';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -16,24 +17,56 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0 ">
       {/* Image and MainHeader */}
-      <div>
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6 ">
         {/* Main Header */}
-        <div>
+        <div className="z-10 mt-32 md:basis-3/5">
           {/* Headings */}
-          <div>
-            <div>
-              <div>
+          <div className="md:-mt-20 ">
+            <div className="relative ">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img src={HomePageText} alt="home-page-text" />
               </div>
             </div>
 
-            <p></p>
+            <p>
+              Unrivaled Gym. Unparalled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </div>
+          {/* {Actions} */}
+          <div>
+            <ActionButton setSelectedPage={setSelectedPage}>
+              Join Now
+            </ActionButton>
+            <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`${SelectedPage.ContactUs}`}
+            >
+              <p>Learn More</p>
+            </AnchorLink>
           </div>
         </div>
 
         {/* Image */}
-        <div></div>
+        <div>
+          <img src={HomePageGraphic} alt="home-page-graphic" />
+        </div>
       </div>
+
+      {/* Sponsers */}
+      {isAboveMediumScreens && (
+        <div>
+          <div>
+            <div>
+              <img src={SponserRedBull} alt="redbull-sponser" />
+              <img src={SponserForbes} alt="forbes-sponser" />
+              <img src={SponserFortune} alt="fortune-sponser" />
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ export default {
         montserrat: ['Montserrat', 'sans-serif'],
       },
       content: {
-        evolvetext: "url('./assests/EvolveText')",
+        evolvetext: "url('./assets/EvolveText.png')",
         abstractwaves: "url('./assets/AbstractWaves.png')",
         sparkles: "url('./assets/Sparkles.png')",
         circles: "url('./assets/Circles.png')",
