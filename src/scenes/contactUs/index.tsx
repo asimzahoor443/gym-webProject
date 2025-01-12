dc01a76b8fe061c0eb92c06f6b4dbb997bfaa950
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2025-01-12 12:41:34 +0500

    feat:update

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -1,7 +1,86 @@
-type Props = {};
+import { SelectedPage } from '@/shared/types';
+import { motion } from 'framer-motion';
+import { useForm } from 'react-hook-form';
+import ContactUsPageGraphic from '@/assets/ContactUsPageGraphic.png';
+import HText from '@/shared/HText';
 
-const ContactUs = (props: Props) => {
-  return <div>index</div>;
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyles = `w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-black`;
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!valid) {
+      e.preventDefault();
+    }
+  };
+  return (
+    <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage)}>
+        {/* Header */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          className="md:w-3/5"
+        >
+          <HText>
+            <span className="text-primary-500">Join Now</span> TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Cras faucibus purus sed lorem vestibulum, ac laoreet orci viverra.
+            Cras commodo neque elementum turpis tincidunt tincidunt. Curabitur a
+            faucibus ante, non suscipit metus. Sed accumsan ex quis lectus
+            scelerisque, eu lacinia dui vestibulum.
+          </p>
+        </motion.div>
+        {/* Forms and Image */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+            className="mt-10 basis-3/5 md:mt-0 "
+          >
+            <form
+              target="_blank"
+              onSubmit={onSubmit}
+              action="https://formsubmit.co/asimzahoor443@gmail.com"
+              method="POST"
+            >
+              <input
+                className={inputStyles}
+                type="text"
+                placeholder="Name"
+                {...register('name', {
+                  required: true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.name && <p></p>}
+            </form>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default ContactUs;
